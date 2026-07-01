dca19b3c735e85aa8ff8c929d0eab648f3112a35
RF-Tar-Railt <rf_tar_railt@qq.com>
2026-07-01 12:26:20 +0800

    feat(frontend): apply layout based on route meta

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <OfflineOverlay />
-  <RouterView />
+  <component :is="layout"><RouterView /></component>
 </template>
 <script setup lang="ts">
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+import Default from "@/layouts/Default.vue";
+import Blank from "@/layouts/Blank.vue";
 import OfflineOverlay from "@/components/OfflineOverlay.vue";
 import { useThemeStore } from "@/stores/theme";
-const theme = useThemeStore();
-theme.init();
+const route = useRoute();
+const layout = computed(() => (route.meta.layout === "blank" ? Blank : Default));
+useThemeStore().init();
 </script>
